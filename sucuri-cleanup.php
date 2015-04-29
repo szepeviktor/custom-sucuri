f07cb74d16fe47a44f74d0792154eff75462bad6
Viktor Szépe <viktor@szepe.net>
2015-04-29 10:20:36 +0200

    Added Log storage path relocation

--- sucuri-cleanup.php
+++ sucuri-cleanup.php
@@ -1,13 +1,19 @@
 <?php
 /*
-Plugin Name: Sucuri Scanner Firewall menu hider
-Description: Hide Firewall menu and Plugin advertisements.
-Version: 1.1.0
+Plugin Name: Sucuri Scanner custom settings
+Description: Hide Firewall menu and Plugin advertisements, relocate datastore path.
+Version: 2.0.0
+Author: Viktor Szépe, Wojtek Szałkiewicz
+License: GNU General Public License (GPL) version 2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+GitHub Plugin URI: https://github.com/szepeviktor/sucuri-cleanup
 */
 
 add_action( 'admin_menu', 'o1_sucuri_remove_firewall', 0 );
 add_filter( 'pre_option_' . 'sucuriscan_ads_visibility', 'o1_sucuri_ads_visibility', 9999 );
 add_filter( 'pre_update_option_' . 'sucuriscan_ads_visibility', 'o1_sucuri_ads_visibility', 9999 );
+add_filter( 'pre_option_' . 'sucuriscan_datastore_path', 'o1_sucuri_datastore_path', 9999 );
+add_filter( 'pre_update_option_' . 'sucuriscan_datastore_path', 'o1_sucuri_datastore_path', 9999 );
 
 function o1_sucuri_remove_firewall() {
 
@@ -19,4 +25,9 @@ function o1_sucuri_remove_firewall() {
 function o1_sucuri_ads_visibility( $value ) {
 
     return 'disabled';
+}
+
+function o1_sucuri_datastore_path( $value ) {
+
+    return WP_CONTENT_DIR . '/sucuri';
 }
