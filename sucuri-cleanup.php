0c9da42773a07d419d428b4dba22e8f0df03e16d
Viktor Szépe <viktor@szepe.net>
2015-05-12 19:22:17 +0200

    Hide Sucuri WAF postbox

--- sucuri-cleanup.php
+++ sucuri-cleanup.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Sucuri Scanner custom settings
 Description: Hide Firewall menu and Plugin advertisements, relocate datastore path.
-Version: 2.0.0
+Version: 2.1.0
 Author: Viktor Szépe
 License: GNU General Public License (GPL) version 2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -10,8 +10,11 @@ GitHub Plugin URI: https://github.com/szepeviktor/sucuri-cleanup
 */
 
 add_action( 'admin_menu', 'o1_sucuri_remove_firewall', 0 );
+add_action( 'admin_enqueue_scripts', 'o1_sucuri_hide_waf_postbox' );
+
 add_filter( 'pre_option_' . 'sucuriscan_ads_visibility', 'o1_sucuri_ads_visibility', 9999 );
 add_filter( 'pre_update_option_' . 'sucuriscan_ads_visibility', 'o1_sucuri_ads_visibility', 9999 );
+
 add_filter( 'pre_option_' . 'sucuriscan_datastore_path', 'o1_sucuri_datastore_path', 9999 );
 add_filter( 'pre_update_option_' . 'sucuriscan_datastore_path', 'o1_sucuri_datastore_path', 9999 );
 
@@ -20,6 +23,16 @@ function o1_sucuri_remove_firewall() {
     global $sucuriscan_pages;
 
     unset( $sucuriscan_pages['sucuriscan_monitoring'] );
+}
+
+function o1_sucuri_hide_waf_postbox( $hook ) {
+
+     if ( 'sucuri-security_page_sucuriscan_hardening' !== $hook ) {
+         return;
+     }
+
+    $style = '.sucuri-security_page_sucuriscan_hardening #poststuff .postbox:nth-of-type(2) {display:none !important}';
+    wp_add_inline_style( 'wp-admin', $style );
 }
 
 function o1_sucuri_ads_visibility( $value ) {
